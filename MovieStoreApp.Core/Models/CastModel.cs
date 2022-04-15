b75015d1ad9878080312ad2d59dedf0e2f541084
Madhusudan Sharma <sharmamadhusudan@hotmail.com>
2022-04-15 14:49:36 -0500

    routing

--- MovieStoreApp.Core/Models/CastModel.cs
+++ MovieStoreApp.Core/Models/CastModel.cs
@@ -13,14 +13,21 @@ namespace MovieStoreApp.Core.Models
 
         [MaxLength(100)]
         [Column(TypeName = "Varchar")]
+        [Required(ErrorMessage ="Cast Name is Required")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage ="Gender is required")]
         public int Gender { get; set; }
+
         [MaxLength(100)]
         [Column(TypeName = "Varchar")]
+        [Required(ErrorMessage ="Tmdb Url is required")]
+        [DataType(DataType.Url)]
         public string TmdbUrl { get; set; }
         [MaxLength(130)]
         [Column(TypeName = "Varchar")]
+        [Required(ErrorMessage ="Cast Profile path is required")]
+        [DataType(DataType.Url)]
         public string ProfilePath { get; set; }
 
         public virtual ICollection<MovieCastModel> MovieCasts { get; set; }
